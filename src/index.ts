ca876a6840518beff46f4a2ce8b8daf27860971c
Walker Sakatch <walkersakatch@gmail.com>
2022-12-15 23:08:16 -0700

    working w cors. Connects to site

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import * as dotenv from 'dotenv';
 dotenv.config();
 //DataDragon Equiv:
@@ -6,8 +7,10 @@ dotenv.config();
 import { getMaps } from "./api/maps/getMaps.js";
 import { getVersion } from "./api/version/getVersion.js";
 import { getWeapons } from "./api/weapons/getWeapons.js";
+import { Weapon } from "./api/weapons/definitions/Weapon.js";
 
 let app = express();
+app.use(cors());
 
 let versionData = await getVersion();
 // console.log(versionData.data.version);
@@ -17,7 +20,7 @@ let mapsData = await getMaps();
 
 let weaponData = await getWeapons('63e6c2b6-4a8e-869c-3d4c-e38355226584');
 // console.log(weaponData.data[0].skins[14])
-// console.log(weaponData.data[0].skins);
+// console.log(weaponData.data[0].skins[0]);
 
 
 app.listen(process.env.PORT, () => {
@@ -26,8 +29,8 @@ app.listen(process.env.PORT, () => {
 
 app.get('/version', async (req, res) => {
     let data = await getVersion();
-    res.statusCode = 404;
-    res.send(data.data, );
+    res.statusCode = 200;
+    res.send(data.data);
 });
 
 app.get('/weapons', async (req, res) => {
