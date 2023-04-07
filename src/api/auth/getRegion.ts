1cf9e809ebb07677bbe9f1305a981d5051d9887c
Walker Sakatch <walkersakatch@gmail.com>
2023-04-07 12:05:41 -0600

    Revert "Revert "can get region of user / convert region into shard""
    This reverts commit 77479f19f4ac06ae3b7d2bfa4b91133525d764fa.

--- src/api/auth/getRegion.ts
+++ src/api/auth/getRegion.ts
@@ -1,8 +1,7 @@
 import { RiotAuthHttpClient } from "../../http/riot-auth-http-client.js";
-import { AuthorizationResponse } from "./definitions/AuthorizationResponse.js";
 import { RegionResponse } from "./definitions/RegionResponse.js";
 
-export async function getRegion(accessToken: string, idToken: string) {
+export async function getRegion(accessToken: string, idToken: string): Promise<RegionResponse> {
     const client = new RiotAuthHttpClient();
     const {status, data} = await client.putAccountRegion(accessToken, idToken);
     return {
