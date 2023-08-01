bbe6a07b5056386e10f3b691be551eca85a1f15b
Walker Sakatch <walkersakatch@gmail.com>
2023-07-31 23:23:00 -0600

    filter out npcs (sova -_-)

--- src/api/agents/getAgents.ts
+++ src/api/agents/getAgents.ts
@@ -3,7 +3,7 @@ import { AgentResponse } from "./definitions/AgentResponse.js";
 
 export async function getAgents(uuid?: string): Promise<AgentResponse> {
     const client = new HttpClient();
-    const url = uuid ? `https://valorant-api.com/v1/agents/${uuid}` : 'https://valorant-api.com/v1/agents';
+    const url = uuid ? `https://valorant-api.com/v1/agents/${uuid}` : 'https://valorant-api.com/v1/agents?isPlayableCharacter=true';
     let {status, data} = await client.get(url);
 
     if(Array.isArray(data)) {
